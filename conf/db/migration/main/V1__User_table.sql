c75bef5d412c24ea2398fa67e629e790da652295
Semenov <anton.semenov@metahouse.ru>
2018-05-01 21:09:19 +0300

    Added host meta table

--- conf/db/migration/main/V1__User_table.sql
+++ conf/db/migration/main/V1__User_table.sql
@@ -1,21 +1,25 @@
 -- Connection must be done as superuser
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
+---- Category
 CREATE TABLE "Category" (
-  id SERIAL NOT NULL PRIMARY KEY,
-  parentId INTEGER NULL REFERENCES "Category" (id) ON UPDATE RESTRICT ON DELETE CASCADE,
+  id SERIAL PRIMARY KEY,
+  parentId INTEGER NULL REFERENCES "Category" (id)
+    ON UPDATE RESTRICT
+    ON DELETE CASCADE,
   name VARCHAR(255) NOT NULL UNIQUE,
   isFinal BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE FUNCTION is_final_category(int) RETURNS BOOLEAN AS $$
 SELECT EXISTS(
-  SELECT id FROM "Category" WHERE id = $1 AND isFinal = TRUE
+    SELECT id FROM "Category" WHERE id = $1 AND isFinal = TRUE
 );
 $$ LANGUAGE sql;
 
+---- User
 CREATE TABLE "User" (
-  id SERIAL NOT NULL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   firstName VARCHAR(255) NOT NULL,
   surname VARCHAR(255) NOT NULL,
   lastName VARCHAR(255) NOT NULL,
@@ -24,16 +28,22 @@ CREATE TABLE "User" (
   googleId VARCHAR(255) NOT NULL UNIQUE,
   isHost BOOLEAN NOT NULL DEFAULT FALSE,
   isBlocked BOOLEAN NOT NULL DEFAULT FALSE,
-  categoryId INTEGER NOT NULL REFERENCES "Category" (id) ON UPDATE RESTRICT ON DELETE RESTRICT,
-  CONSTRAINT FinalCategory_check CHECK (is_final_category(categoryId))
+  categoryId INTEGER NOT NULL REFERENCES "Category" (id)
+    ON UPDATE RESTRICT
+    ON DELETE RESTRICT
+    CHECK (is_final_category(categoryId))
 );
 
-CREATE TYPE visit_status AS ENUM ('pending', 'finished', 'cancelledByUser', 'cancelledByHost');
+CREATE FUNCTION is_host_user(int) RETURNS BOOLEAN AS $$
+SELECT EXISTS(
+    SELECT id FROM "User" WHERE id = $1 AND isHost = TRUE
+)
+$$ LANGUAGE sql;
 
 CREATE FUNCTION is_active_host_user(int) RETURNS BOOLEAN AS $$
-  SELECT EXISTS(
+SELECT EXISTS(
     SELECT id FROM "User" WHERE id = $1 AND isHost = TRUE AND isBlocked = FALSE
-  )
+)
 $$ LANGUAGE sql;
 
 CREATE FUNCTION is_active_user(int) RETURNS BOOLEAN AS $$
@@ -42,14 +52,29 @@ SELECT EXISTS(
 )
 $$ LANGUAGE sql;
 
+---- Host Meta
+CREATE TABLE "HostMeta" (
+  id INTEGER PRIMARY KEY REFERENCES "User"
+    ON UPDATE RESTRICT
+    ON DELETE CASCADE,
+  appointmentInterval INTERVAL DAY NOT NULL DEFAULT '31 days'
+);
+
+---- Appointment
+CREATE TYPE visit_status AS ENUM ('pending', 'finished', 'cancelledByUser', 'cancelledByHost');
+
 CREATE TABLE "Appointment" (
-  id BIGSERIAL NOT NULL PRIMARY KEY,
-  hostId INT NOT NULL REFERENCES "User" (id) ON UPDATE RESTRICT ON DELETE RESTRICT,
-  visitorId INT NOT NULL REFERENCES "User" (id) ON UPDATE RESTRICT ON DELETE RESTRICT,
+  id BIGSERIAL PRIMARY KEY,
+  hostId INT NOT NULL REFERENCES "User" (id)
+    ON UPDATE RESTRICT
+    ON DELETE RESTRICT
+    CHECK (is_active_host_user(hostId)),
+  visitorId INT NOT NULL REFERENCES "User" (id)
+    ON UPDATE RESTRICT
+    ON DELETE RESTRICT
+    CHECK (is_active_user(visitorId)),
   date TIMESTAMP NOT NULL,
   status visit_status NOT NULL DEFAULT 'pending',
-  CONSTRAINT TimeTable_unique UNIQUE (hostId, date),
-  CONSTRAINT HostActive_check CHECK (is_active_host_user(hostId)),
-  CONSTRAINT VisitorActive_check CHECK (is_active_user(visitorId))
+  CONSTRAINT TimeTable_unique UNIQUE (hostId, date)
 )
 
