5b2bd83e29adbb02fe36a27b546dc592fbe32303
Semenov <anton.semenov@metahouse.ru>
2018-05-06 23:28:31 +0300

    Small format fix

--- app/controllers/formats/HostData.scala
+++ app/controllers/formats/HostData.scala
@@ -2,4 +2,4 @@ package controllers.formats
 
 import db.data.User.UserId
 
-case class HostData(id: UserId, firstName: String, surName: String, lastName: String)
+case class HostData(id: UserId, firstName: String, surname: String, patronymic: String)
