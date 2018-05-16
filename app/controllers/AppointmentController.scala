6ba8fd5150b0f9f26965229eda5a97b422c194d8
Антон Семенов <anton.semenov@metahouse.ru>
2018-05-17 00:08:02 +0300

    Modified routes and appointment controller

--- app/controllers/AppointmentController.scala
+++ app/controllers/AppointmentController.scala
@@ -3,38 +3,35 @@ package controllers
 import controllers.errors.ErrorResponses
 import controllers.formats.HttpFormats._
 import controllers.formats.request.{AppointmentsRequest, CreateAppointmentRequest}
+import controllers.util.ControllerUtils.extractJsObject
 import db.ConnectionUtils
-import db.data.User.UserId
-import db.data.{Appointment, AppointmentData}
+import db.data.Appointment
 import doobie.implicits._
 import javax.inject.{Inject, Singleton}
-import org.joda.time.DateTime
 import play.api.mvc.{AbstractController, ControllerComponents}
-import controllers.util.ControllerUtils.extractJsObject
 
 @Singleton
 class AppointmentController @Inject()(cc: ControllerComponents, cu: ConnectionUtils) extends AbstractController(cc) {
 
   //todo unique constraint errors
 
-  def create(hostId: UserId, visitorId: UserId) = Action { implicit r =>
+  def create = Action { implicit r =>
     extractJsObject[CreateAppointmentRequest] { req =>
 
-      if (hostId == visitorId) {
-        ErrorResponses.invalidHostUser(hostId)
+      if (req.hostId == req.visitorId) {
+        ErrorResponses.invalidHostUser(req.hostId)
       } else {
-        val dateTime = DateTime.now()
-        val date = dateTime.toLocalDate
-        val start = dateTime.toLocalTime
-        val end = start.plusHours(3)
-        val appointment = AppointmentData(req.hostId, req.visitorId, req.date, req.start, end)
 
-        val tr = for {
-          id <- Appointment.insert(appointment)
-          a <- Appointment.selectById(id)
-        } yield a
+        //todo check date for schedules?
 
-        Created(tr.transact(cu.transactor).unsafeRunSync().toJson)
+        Appointment.createBySchedule(req.hostId, req.visitorId, req.scheduleId, req.isCustom, req.date, req.start)
+          .transact(cu.transactor)
+          .unsafeRunSync() match {
+
+            case Some(_) => Created
+
+            case None => BadRequest //todo error
+        }
       }
     }
   }
